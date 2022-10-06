8380376d60ea4f904613ca521847fc32a29c6190
dkibrahi <dkibrahi@umich.edu>
2022-10-06 17:07:48 -0400

    base project with starter code

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # clarafied
+A website made to increase interactivity between the student and professor as the semester progresses. 
---
+++ src/pages/home/Home.js
@@ -0,0 +1,10 @@
+// styles
+import styles from './Home.module.css';
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Create a new post</h2>
+    </div>
+  )
+}
