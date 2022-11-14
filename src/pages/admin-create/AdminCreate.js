4332addc044300c8576d5a3cf5946660a2d3cb7f
dkibrahi <dkibrahi@umich.edu>
2022-11-14 14:50:39 -0500

    added basic functionality for creating post

---
+++ src/pages/admin-create/AdminCreate.js
@@ -0,0 +1,83 @@
+// react imports
+import { projFirestore } from '../../firebase/config';
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+// icons
+import { Alert, Card, TextField } from '@mui/material';
+
+// components
+import CreatePost from '../../components/create-post/CreatePost';
+
+// styles
+import styles from './AdminCreate.module.css';
+
+export default function AdminCreate() {
+    const history = useHistory();
+
+    const [newTitle, setNewTitle] = useState('');
+    const [newContent, setNewContent] = useState('');
+    const [created, setCreated] = useState(true); 
+    const [failedToCreate, setFailedToCreate] = useState(false);
+
+    const [post, setPost] = useState(
+        {
+            title: '',
+            content: ''
+        }
+    );
+
+    const handleCancel = () => {
+        history.push('/home');
+    }
+
+    const handleSave = async () => {
+        const today = new Date().toISOString().slice(0, 10);
+
+        const doc = { 
+            author: 'placeholder',
+            title: newTitle,
+            content: newContent,
+            date: today 
+        };
+
+        try {
+            await projFirestore.collection('posts').add(doc);
+             history.push('/home');
+            // history.push(`/posts${newTitle}`); will send user to their post eventually
+        } catch(err) {
+            console.log(err);
+        }
+
+    }
+
+    return (
+        <>
+            <Card variant="outlined" className={styles.card}>
+                <CreatePost
+                    post={post}
+                    setView={handleCancel}
+                    setNewContent={setNewContent}
+                    handleSave={handleSave}>
+                    <TextField
+                        required
+                        id="outlined-required"
+                        onChange={(e) => setNewTitle(e.target.value)}
+                        label="Title"
+                        defaultValue={post.title}/>
+                </CreatePost>
+            </Card>
+
+            {/* {created && 
+                <Alert 
+                    severity={'success'}
+                    title={'Created Post!'}
+                    description={'The post was created! Users will be able to reply in just a moment....'}/>
+            } */}
+
+            {/* {failedToCreate && 
+                <Alert />
+            } */}
+        </>
+  )
+}
