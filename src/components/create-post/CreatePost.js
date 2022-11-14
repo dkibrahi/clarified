c3bdda69cc4a8ff8d25287675f566a2e5c48bc45
walkevy <111020770+walkevy@users.noreply.github.com>
2022-11-13 19:28:10 -0500

    created post

--- src/components/create-post/CreatePost.js
+++ src/components/create-post/CreatePost.js
@@ -1,27 +1,65 @@
 // react imports
 import { useState } from 'react';
+import { projFirestore } from '../../firebase/config';
+import Button from '@material-ui/core/Button';
 
-// images
-import defaultProfileMale from '../../images/default-profile-male.png';
-
-// styles
 import styles from './CreatePost.module.css';
 
 export default function CreatePost() {
-    const [post, setPost] = useState('');
-
+    const[author, setAuthor] = useState('John Doe');
+    const[content, setContent] = useState('');
+    const[title, setTitle] = useState('');
+    var time = new Date().toLocaleTimeString();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const posts = {author, content, title, time};
+        console.log(posts);
+        try {
+            await projFirestore.collection('posts').add(posts).set({
+                author: author,
+                content: content,
+                title: title,
+                time: time
+            })
+        } catch(err) {
+            console.log(err);
+        }
+    }
     return (
-        <form className={styles["create-post"]}>
-            <label className={styles["main-input"]}>
-                <img src={defaultProfileMale} alt='default male profile'/>
-                <input 
-                    type='text'
-                    onChange={(e) => setPost(e.target.value)}
-                    value={post}
-                    placeholder="Enter your thoughts..."
-                />
-            </label>
-            <button className='btn'>Post</button>
-        </form>
+   <div className='create'>
+    <h2 className='page-title'>Create Post</h2>
+    
+    <form onSubmit={handleSubmit}>
+        <label>
+            <span>Author:</span>
+            <input type ="text" 
+            onChange={(e)=> setAuthor(e.target.value)} 
+            value={author}
+            required
+        />
+        </label>
+
+        <label>
+            <span>Content:</span>
+            <textarea
+            onChange={(e)=> setContent(e.target.value)}
+            value={content}
+            required
+            />
+        </label>
+
+        <label>
+            <span >Title:</span>
+            <input type = "text"
+            onChange={(e)=> setTitle(e.target.value)}
+            value={title}
+            required
+            />
+        </label>
+
+        <Button color="secondary" type='submit'>Create Post</Button>
+
+    </form>
+   </div>
   )
 }
